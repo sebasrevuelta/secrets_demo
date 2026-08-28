8c952f67674968a5c8a808d30587a24cc2109d8c
sebasrevuelta <122784773+sebasrevuelta@users.noreply.github.com>
2026-08-28 13:14:51 +0200

    Update file.py

--- file.py
+++ file.py
@@ -7,7 +7,7 @@ credentials.password1 = ""
 credentials.a = 1
 
 # ruleid: mongo
-connection_string = "mongodb+srv://username:{}@{mongodbdb_host}/?retryWrites=true&w=majority".format(credentials.password)
+connection_string = "mongodb+srv://username:{}@{mongodbdb_host}/?retryWrites=true&w=majority".format(credentials.password) # nosemgrep
 # ruleid: mongo
 connection_string = "mongodb://{}:{}@{mongodbdb_host}/?retryWrites=true&w=majority".format(credentials.username,credentials.password)
 # ok: mongo
